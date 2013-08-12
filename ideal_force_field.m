80f57a4f77cb712b1ab892dfe97fc7eda7a00993
Tobias Klauser <tklauser@distanz.ch>
2013-08-12 14:43:56 +0200

    Rename ideal_weights.m to ideal_force_field.m

--- ideal_force_field.m
+++ ideal_force_field.m
@@ -1,4 +1,5 @@
-function [u,v] = ideal_weights(minX, maxX, minY, maxY)
+% "Ideal" force field to result in fixating behavior
+function [u,v] = ideal_force_field(minX, maxX, minY, maxY)
     x = linspace(minX,maxX,5)*(-1);
     y = linspace(minY,maxY,5);
     [u,v] = meshgrid(x,y);
