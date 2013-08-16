29e0e68ed39f31fe5b4b92816cff2a2f2118ae95
Tobias Klauser <tklauser@distanz.ch>
2013-08-16 15:44:36 +0200

    scripts/ideal_force_field.m: Allow to specify # of rows/columns

--- ideal_force_field.m
+++ ideal_force_field.m
@@ -1,7 +1,13 @@
 % "Ideal" force field to result in fixating behavior
-function [u,v] = ideal_force_field(minX, maxX, minY, maxY)
-    x = linspace(minX,maxX,5)*(-1);
-    y = linspace(minY,maxY,5);
+function [u,v] = ideal_force_field(minX, maxX, minY, maxY, nRows, nCols)
+    % if not specified use default values of 5 for nRows and nCols
+    if nargin < 6
+        nRows = 5;
+        nCols = 5;
+    end
+
+    x = linspace(minX,maxX,nCols)*(-1);
+    y = linspace(minY,maxY,nRows);
     [u,v] = meshgrid(x,y);
     u = flipud(u);
     v = flipud(v);
