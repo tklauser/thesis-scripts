c4fbe790b01c52860928b6a74b113aade53c3cd6
Tobias Klauser <tklauser@distanz.ch>
2014-01-08 16:14:47 +0100

    scripts: plot_output: Move common code to own function

--- plot_output.m
+++ plot_output.m
@@ -49,6 +49,25 @@ cmap = interp1(linspace(0, 1, size(colormap, 1)), colormap, linspace(0.0,0.9,T))
 % reverse, so the more recent data points are darker
 cmap = flipud(cmap);
 
+T = T -1;
+plot_output_1d(T, nRows, nCols, nOutputs, outputs_x, cmap);
+figure;
+plot_output_1d(T, nRows, nCols, nOutputs, outputs_y, cmap);
+
+% h = colorbar;
+% set(h, 'Position', [ .8314 .11 .0581 .8150 ])
+% for i=1:nInputs
+%     pos = get(ax(i), 'Position');
+%     set(ax(i), 'Position', [pos(1) pos(2) 0.85*pos(3) pos(4)]);
+% end
+
+end % function plot_output
+
+function [] = plot_output_1d(T, nRows, nCols, nOutputs, outputs, cmap)
+
+nInputs = nRows * nCols;
+dt = max(floor(T / nInputs), 1);
+
 N = 1;
 if mod(nInputs, 7) == 0
     N = 7;
@@ -58,61 +77,42 @@ elseif mod(nInputs, 3) == 0
     N = 3;
 end
 
-figure(1);
-
-dt = max(floor(T / 24), 1);
-
 for i=1:nInputs
-    ax(i) = subplot(N, nInputs/N, nInputs - i + 1);
-
-    for t=1:dt:T
-        hold on;
-        %subplot(T, nInputs, (t-1)*T + i);
-        lw = 0.5;
-        if t == T || t + dt > T
-            lw = 2.0;
-        end
-        plot(outputs_x(t,:,i), 'LineWidth', lw, 'Color', cmap(t,:));
-        set(gca, 'FontSize', 11);
-        set(gca, 'FontName', 'Times New Roman');
-        %xlabel('output neuron', 'FontSize', 18);
-        %ylabel('activity', 'FontSize', 18);
-        hold off;
-    end
-    axis([1 nOutputs -0.5 2.5]);
-    axis square;
-    title(sprintf('input x%d', i - 1));
-end
-
-figure(2);
-
-for i=1:nInputs
-    ax(i) = subplot(N, nInputs/N, nInputs - i + 1);
+    subplot(N, nInputs/N, nInputs - i + 1);
 
     for t=1:dt:T
         hold on;
 
         lw = 0.5;
         if t == T || t + dt > T
-            lw = 2.0;
+            lw = 1.25;
         end
-        plot(outputs_y(t,:,i), 'LineWidth', lw,  'Color', cmap(t,:));
-        set(gca, 'FontSize', 11);
+
+        plot(outputs(t,:,i), 'LineWidth', lw, 'Color', cmap(t,:));
+        set(gca, 'FontSize', 8);
         set(gca, 'FontName', 'Times New Roman');
-        %xlabel('output neuron', 'FontSize', 18);
-        %ylabel('activity', 'FontSize', 18);
+        if i <= nCols
+            xlabel('output unit', 'FontSize', 11);
+        end
+        if mod(i, nCols) == 0
+            ylabel('activity', 'FontSize', 11);
+        else
+            set(gca, 'YTickLabel', [])
+        end
         hold off;
     end
-    axis([1 nOutputs -0.5 2.5]);
+    axis([1 nOutputs -0.5 2.75]);
     axis square;
-    title(sprintf('input y%d', i - 1));
+    set(gca,'XTick',1:10);
+    if i <= nCols
+        %set(gca,'XTickLabel',['0';' ';' ';' ';' ';' ';' ';' ';' ';'9']);
+        set(gca,'XTickLabel',0:9);
+    else
+        set(gca,'XTickLabel',[]);
+    end
+    %title(sprintf('input x_{%d}', i - 1));
 end
-
-% h = colorbar;
-% set(h, 'Position', [ .8314 .11 .0581 .8150 ])
-% for i=1:nInputs
-%     pos = get(ax(i), 'Position');
-%     set(ax(i), 'Position', [pos(1) pos(2) 0.85*pos(3) pos(4)]);
-% end
 
 tightfig;
+
+end % function plot_output_1d
